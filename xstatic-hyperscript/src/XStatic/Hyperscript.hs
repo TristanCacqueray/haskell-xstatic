a2d4c9623e3c6a0c1066f195017e1a9ad6b9231c
Tristan Cacqueray <tdecacqu@redhat.com>
2022-09-03 11:51:43 +0000

    Add hyperscript sse

--- xstatic-hyperscript/src/XStatic/Hyperscript.hs
+++ xstatic-hyperscript/src/XStatic/Hyperscript.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module XStatic.Hyperscript (hyperscript) where
+module XStatic.Hyperscript (hyperscript, hyperscriptSSE) where
 
 import Paths_xstatic_hyperscript (version)
 import XStatic (XStaticFile (..), embedFile)
@@ -11,6 +11,15 @@ hyperscript =
     XStaticFile
         { name = "hyperscript.min.js"
         , content = $(embedFile "data/_hyperscript.min.js.gz")
+        , contentType = "text/javascript; charset=UTF-8"
+        , contentVersion = version
+        }
+
+hyperscriptSSE :: XStaticFile
+hyperscriptSSE =
+    XStaticFile
+        { name = "eventsource.js"
+        , content = $(embedFile "data/eventsource.js.gz")
         , contentType = "text/javascript; charset=UTF-8"
         , contentVersion = version
         }
