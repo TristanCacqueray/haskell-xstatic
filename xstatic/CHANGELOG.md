fd54f65c1010c9c44ea0265280cde11a5b35e738
Tristan Cacqueray <tdecacqu@redhat.com>
2022-09-08 00:21:37 +0000

    Enable StrictData for XStaticFile

--- xstatic/CHANGELOG.md
+++ xstatic/CHANGELOG.md
@@ -3,6 +3,7 @@
 ## 0.2.0
 
 - Add xstaticMiddleware
+- Enable StrictData
 
 ## 0.1.0
 
--- xstatic/src/XStatic.hs
+++ xstatic/src/XStatic.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StrictData #-}
 
 module XStatic (
     -- * xstatic api
