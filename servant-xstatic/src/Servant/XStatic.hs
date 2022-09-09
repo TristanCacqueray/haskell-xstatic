618fa7fd652e7c3e3ce0d17d98c68fa834c37250
Tristan Cacqueray <tdecacqu@redhat.com>
2022-09-09 17:04:29 +0000

    Add documentation for servant-xstatic

--- servant-xstatic/src/Servant/XStatic.hs
+++ servant-xstatic/src/Servant/XStatic.hs
@@ -1,5 +1,19 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 
+{- | Serve a XStaticFiles in your servant api:
+
+@
+type ServerAPI =
+    Get '[HTML] (Html ())
+        :\<|\> "xstatic" :> Raw
+
+server :: Server ServerAPI
+server = pure mempty \<|\> xstaticServant xfiles
+  where
+    xfiles = [XStatic.htmx]
+@
+-}
+
 module Servant.XStatic (xstaticServant) where
 
 import Servant.API (Raw)
@@ -7,5 +21,6 @@ import Servant.Server (ServerT, Tagged (..))
 import XStatic (XStaticFile)
 import XStatic qualified
 
+-- | Create a 'Raw' handler.
 xstaticServant :: [XStaticFile] -> ServerT Raw m
 xstaticServant = Tagged . XStatic.xstaticApp
