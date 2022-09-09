205506d8b53af9f9e448bfd0e2eadec349058eca
Tristan Cacqueray <tdecacqu@redhat.com>
2022-09-09 17:10:33 +0000

    Add documentation for lucid-xstatic

--- lucid-xstatic/src/Lucid/XStatic.hs
+++ lucid-xstatic/src/Lucid/XStatic.hs
@@ -1,5 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+{- | Add XStaticFile to Html document.
+
+@
+indexHtml :: Html ()
+indexHtml = do
+    doctypehtml_ do
+        head_ do
+            meta_ [charset_ "utf-8"]
+            meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1.0"]
+            xstaticScripts xfiles
+  where
+    xfiles = XStatic.htmx : XStatic.xterm
+@
+-}
 module Lucid.XStatic (xstaticScripts) where
 
 import Data.Foldable (traverse_)
@@ -7,6 +21,7 @@ import Data.Text.Encoding
 import Lucid
 import XStatic
 
+-- | Adds 'script_' and 'link_' for javascript and css files.
 xstaticScripts :: [XStaticFile] -> Html ()
 xstaticScripts = traverse_ xrender
   where
